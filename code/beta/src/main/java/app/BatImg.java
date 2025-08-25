e3ba6e27723c863e515dc69b95e8c20e59f04b8a
nskowron <nel.skowronek@gmail.com>
2025-08-25 10:28:04 +0200

    cleanup

--- code/beta/src/main/java/app/BatImg.java
+++ code/beta/src/main/java/app/BatImg.java
@@ -3,10 +3,10 @@ package app;
 import javafx.scene.image.Image;
 
 public class BatImg {
-    public static Image idle = new Image("file:img/idle.png");
+    public static Image idle = new Image("img/idle.png");
     public static Image[] tap = {
-        new Image("file:img/tap_left.png"),
-        new Image("file:img/tap_right.png")
+        new Image("img/tap_left.png"),
+        new Image("img/tap_right.png")
     };
 
     private BatImg() throws InstantiationError {
